335a6388023775c7f57e195e4c10ba923162fa5d
ackottsi <kottsickandrewc@johndeere.com>
2021-01-09 13:49:05 -0600

    added logic to cap number of choices that will be entered to playerChoice Array

--- script.js
+++ script.js
@@ -3,14 +3,30 @@ console.log("javaScript Link Works!");
 //Need to declare two players
 let playerOne=true;
 let playerTwo=false;
+let clickCounter=0;
 //Create way for players to make choices (Listener event and place to store players sequence)
 
+//test array:
+const testingArray=["red","blue","green"];
+
+
+
+const playerChoice=[];
 const gameButtons=document.querySelectorAll(".button");
 for (let i=0; i<gameButtons.length; i++){
     console.log(gameButtons[i]);
     gameButtons[i].addEventListener("click",function(e){
         e.preventDefault();
         console.log(gameButtons[i]);
+        console.log(gameButtons[i].id)
+    
+        if(playerOne===true && clickCounter<testingArray.length){
+        playerChoice.push(gameButtons[i].id)
+        clickCounter++;
+        console.log("players choice array: "+playerChoice)}
+
+        
+    
     })
 }
 
