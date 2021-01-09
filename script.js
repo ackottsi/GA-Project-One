6ade8188400a73498e7d57d308ac2b2ed4a887f8
ackottsi <kottsickandrewc@johndeere.com>
2021-01-09 14:54:25 -0600

    having logic to test against an array after it is entered

--- script.js
+++ script.js
@@ -4,10 +4,32 @@ console.log("javaScript Link Works!");
 let playerOne=true;
 let playerTwo=false;
 let clickCounter=0;
+let match=true;
 //Create way for players to make choices (Listener event and place to store players sequence)
 
 //test array:
-const testingArray=["red","blue","green"];
+const testingArray=["red","blue","green","yellow","red"];
+
+//Function to compare user array and test array
+//IF Array value is NOT equal, STOPPING CONDITION. else if ARRAY values 
+//are equal AND test , continue 
+function compareResults(){
+
+    console.log("test Arr: "+testingArray);
+    console.log("player choice Arr "+playerChoice);
+ const a=testingArray.toString();
+ const b=playerChoice.toString();   
+ console.log(a);
+ console.log(b);
+if(a===b){
+    console.log("the arrays matches")
+    
+}
+else{
+    console.log("the arrays do not match")
+    
+}
+}
 
 
 
@@ -20,12 +42,22 @@ for (let i=0; i<gameButtons.length; i++){
         console.log(gameButtons[i]);
         console.log(gameButtons[i].id)
     
-        if(playerOne===true && clickCounter<testingArray.length){
-        playerChoice.push(gameButtons[i].id)
-        clickCounter++;
-        console.log("players choice array: "+playerChoice)}
 
-        
+        //below code will all player to enter choices until the number
+        //of choices is equal to the number of choices in the test array
+        if(playerOne===true && clickCounter<testingArray.length){
+        playerChoice.push(gameButtons[i].id);
+        clickCounter++;
+       
+        console.log("players choice array: "+playerChoice);
+     
+        //create function that will compare array values to invoke here;
+
+
+    }
+
+if(playerChoice.length==testingArray.length)
+compareResults();
     
     })
 }
