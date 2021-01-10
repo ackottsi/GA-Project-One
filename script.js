f711781730efb02acf6f3ad01a4c64472a5539f7
ackottsi <kottsickandrewc@johndeere.com>
2021-01-10 13:05:24 -0600

    automated flashing working for all colors on test array.  need to add delay between flashes on automated sequence

--- script.js
+++ script.js
@@ -127,16 +127,40 @@ console.log(automatedArray)
 
 function showSequence(arrSequence){
     for (let i=0; i<arrSequence.length; i++){
+        const sequenceSpeed=setTimeout(function(){
         console.log(arrSequence);
-    if(arrSequence[i].toString()=="red"){
-        gameButtons[0].style.backgroundColor="red";
-        const timer2=setTimeout(function(){
-            gameButtons[0].style.backgroundColor="black";    
-            console.log("timer2 ")},300);
-           }
+        if(arrSequence[i].toString()=="red"){
+            gameButtons[0].style.backgroundColor="red";
+            const timer2=setTimeout(function(){
+                gameButtons[0].style.backgroundColor="black";    
+                    console.log("timer2 ")},300);
         }
 
-    }
+        if(arrSequence[i].toString()=="blue"){
+            gameButtons[1].style.backgroundColor="blue";
+            const timer2=setTimeout(function(){
+                gameButtons[1].style.backgroundColor="black";    
+                    console.log("timer2 ")},300);
+        }   
+
+        if(arrSequence[i].toString()=="green"){
+            gameButtons[2].style.backgroundColor="green";
+            const timer2=setTimeout(function(){
+                gameButtons[2].style.backgroundColor="black";    
+                    console.log("timer2 ")},300);
+        }
+
+        if(arrSequence[i].toString()=="yellow"){
+            gameButtons[3].style.backgroundColor="yellow";
+            const timer2=setTimeout(function(){
+                gameButtons[3].style.backgroundColor="black";    
+                    console.log("timer2 ")},300);
+        }  
+    //should control time between displaying sequence flash     
+    },1000);
+}
+
+}
 
     const generator=document.querySelector("#sequence_button");
     generator.addEventListener("click",function(){
